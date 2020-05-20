0c8c56ae6b1f0600c686bd965b1a6f5d5ee870fe
sunmoon <i@liming.me>
2020-05-20 18:17:51 +0800

    功能优化

--- box.py
+++ box.py
@@ -22,6 +22,7 @@ class IsScp(QWidget):
         self.timer = QtCore.QBasicTimer()
         self.step = 0
         self.local_file_size = 0
+        self.auth_key_file_size = 0
         self.local_file_basename = ""
         self.local_account_info_db = os.getcwd()+"/info.db"
         self.account_info = {
@@ -41,7 +42,19 @@ class IsScp(QWidget):
         self.remote_file_ipt = QLineEdit(self)
         self.auth_type_pwd = QRadioButton(self)
         self.auth_type_key = QRadioButton(self)
+        self.auth_key_ipt = QLineEdit(self)
+        self.auth_key_btn = QPushButton(self)
+        self.auth_key_widget = QWidget(self)
         self.init_ui()
+
+    def draw_auth_key_file_select(self):
+        box = QHBoxLayout()
+        box.addWidget(self.auth_key_ipt)
+        self.auth_key_btn.setText("...")
+        box.addWidget(self.auth_key_btn)
+        self.auth_key_btn.clicked.connect(self.select_auth_key_file)
+        self.auth_key_widget.setLayout(box)
+        return self.auth_key_widget
 
     def draw_local_file_select(self):
         box = QHBoxLayout()
@@ -73,6 +86,28 @@ class IsScp(QWidget):
         print("文件大小: ", self.local_file_size)
         self.local_file_ipt.setText(selected_file)
 
+    def select_auth_key_file(self):
+        selected_file, file_type = QFileDialog.getOpenFileName(self,
+                                                               "选取文件",
+                                                               os.getcwd(),  # 起始路径
+                                                               "All Files (*);;Text Files (*.txt)"
+                                                               )  # 设置文件扩展名过滤,用双分号间隔
+
+        if selected_file == "":
+            print("\n取消选择")
+            return
+
+        print("\n你选择的文件为:")
+        print(selected_file)
+        print("文件筛选器类型: ", file_type)
+        # 获取文件大小
+        self.auth_key_file_size = os.path.getsize(selected_file)
+        if self.auth_key_file_size <= 0:
+            self.alert("选择的文件太小啦")
+            return
+        print("文件大小: ", self.auth_key_file_size)
+        self.auth_key_ipt.setText(selected_file)
+
     def draw_auth_type(self):
         box = QHBoxLayout()
         self.auth_type_key.setText("密钥")
@@ -85,9 +120,9 @@ class IsScp(QWidget):
 
     def on_radio_button_toggled(self):
         if self.auth_type_pwd.isChecked():
-            print("is pwd type")
+            self.auth_key_widget.hide()
         elif self.auth_type_key.isChecked():
-            print("is user key type")
+            self.auth_key_widget.show()
 
     def draw_form(self):
         self.form_layout.setAlignment(QtCore.Qt.AlignLeft)
@@ -98,6 +133,7 @@ class IsScp(QWidget):
         self.form_layout.addRow("请输入服务器ip:", self.ip_input)
         self.form_layout.addRow("请输入端口:", self.port_input)
         self.form_layout.addRow("请选择验证方式:", self.draw_auth_type())
+        self.form_layout.addRow("请选择证书:", self.draw_auth_key_file_select())
         self.form_layout.addRow("请输入登录账号:", self.user_input)
         self.form_layout.addRow("请输入登录密码:", self.pwd_input)
         self.form_layout.addRow("请选择要上传的文件:", self.draw_local_file_select())
